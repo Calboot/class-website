33dc8c8469838cbdabfdf7d221b8fb3cce12c867
欧阳宗润 <ouyangzongrun@qq.com>
2022-01-26 10:09:48 +0800

    typing 增加排行榜

--- monkey_app.py
+++ monkey_app.py
@@ -1,6 +1,19 @@
-from flask import Blueprint, render_template
+import pymongo
+from flask import Blueprint, render_template, session
+
+from user_app import user_app
+
 monkey_app = Blueprint('monkey_app', __name__)
 @monkey_app.route('/monkey')
 def index():
-    return render_template('monkey/index.html')
+    username = session.get("username")
+    if user_app.check_login():
+        return render_template('user/login.html', t_error='请登录')
+    if user_app.check_user():
+        return render_template('user/login.html', t_error='此账号已被封禁', t_color=1)
+    client = pymongo.MongoClient("mongodb://localhost:27017")
+    db_game = client['db_user']
+    c_game = db_game['game']
+    scoreList = c_game.find({"name": "monkey"}).sort("score", pymongo.DESCENDING)
+    return render_template('monkey/index.html', t_scoreList=scoreList, t_username=username)
 
--- typing_app.py
+++ typing_app.py
@@ -1,3 +1,4 @@
+import pymongo
 from flask import render_template, request, redirect, session, Blueprint
 import user_app
 typing_app = Blueprint('typing_app', __name__)
@@ -5,8 +6,13 @@ typing_app = Blueprint('typing_app', __name__)
 
 @typing_app.route('/typing')
 def index():
+    username = session.get("username")
     if user_app.check_login():
         return render_template('user/login.html', t_error='请登录')
     if user_app.check_user():
         return render_template('user/login.html', t_error='此账号已被封禁', t_color=1)
-    return render_template('typing/index.html')
+    client = pymongo.MongoClient("mongodb://localhost:27017")
+    db_game = client['db_user']
+    c_game = db_game['game']
+    scoreList = c_game.find({"name": "typing"}).sort("score", pymongo.DESCENDING)
+    return render_template('typing/index.html', t_scoreList=scoreList, t_username=username)
