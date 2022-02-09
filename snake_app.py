36eb5aa7068b609cd64e4a12d48f9abe7c66ca11
欧阳宗润 <ouyangzongrun@qq.com>
2022-02-09 20:48:14 +0800

    贪吃蛇加排名

--- snake_app.py
+++ snake_app.py
@@ -1,6 +1,20 @@
-from flask import Blueprint, render_template
+import pymongo
+from flask import Blueprint, render_template, session
+import user_app
+
 snake_app = Blueprint('snake_app', __name__)
+
+client = pymongo.MongoClient("mongodb://localhost:27017")
+db_game = client['db_web']
+c_game = db_game['game']
+
 @snake_app.route('/snake')
 def index():
-    return render_template('snake/index.html')
+    if user_app.check_login():
+        return render_template('user/login.html', t_error='请登录')
+    if user_app.check_user():
+        return render_template('user/login.html', t_error='此账号已被封禁', t_color=1)
+    username = session.get("username")
+    scoreList = c_game.find({"name": "snake"}).sort("score", pymongo.DESCENDING)
+    return render_template('snake/index.html', t_scoreList=scoreList, t_username=username)
 
